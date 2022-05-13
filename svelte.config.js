35d10cf1a944407cd7c628f8e007f14506aa7a45
aizichen <1098679804@qq.com>
2022-05-13 18:01:53 +0800

    add build configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
-
+		// adapter: adapter(),
+		adapter: node({
+			out: 'dist'
+		}),
 		// Override http methods in the Todo forms
 		methodOverride: {
 			allowed: ['PATCH', 'DELETE']
